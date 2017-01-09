97fd92a1f198ad1f53382d7cb7f55c0b05a65e2f
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-01-09 09:19:27 -0600

    completed

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // your code here!
+
+
+function calculateWords () {
+
+	$('#js-submit').submit(function (event) {
+    	event.preventDefault();
+
+
+		var txt = $('#user-text').val();
+		var words = txt.split(" ");
+		var sentences = txt.split(".");
+		var sentenceSum = 0;
+		var wordsSum = 0;
+		var distinctWords = [];
+
+
+		for (var i = sentences.length - 1; i >= 0; i--) {
+			sentenceSum += sentences[i].length;
+		}
+
+		for (var i = words.length - 1; i >= 0; i--) {
+			wordsSum += words[i].length;
+		}
+
+  		for (var i=0; i<words.length; i++) {
+    		if (distinctWords.indexOf(words[i]) === -1) {
+      			distinctWords.push(words[i]);
+    		}
+  		}
+
+        $('.js-result').removeClass('hidden');
+		$('.js-word-count').text(words.length);
+		$('.js-unique-word-count').text(distinctWords.length);
+		$('.js-word-length').text((wordsSum/words.length).toFixed(2));
+		$('.js-sentence-length').text((sentenceSum/sentences.length).toFixed(2));
+	});
+}
+
+$(document).ready(calculateWords);
